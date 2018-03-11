f0b07dfd9510121723a98bf7767cb8dc44162436
Adam D. White <adamwhite82@gmail.com>
2018-03-11 01:04:27 -0600

    jQ filter per room properly working,,,

--- loom_decor.js
+++ loom_decor.js
@@ -6,8 +6,20 @@ console.log('loom decor js')
 var $rooms = $('input[name="roomcheckbox"]');
 
 
+// $rooms.change(function() {
+//     var $checked = $rooms.filter(':checked');
+//     console.log($checked.val());
+//     console.log("goodbye world");
+// });
+
 $rooms.change(function() {
     var $checked = $rooms.filter(':checked');
-    console.log($checked.val());
-    console.log("goodbye world");
+    if ($checked.val() == 'kitchen') {
+        $.show('.kitchen-class');
+        $.hide('#');
+
+    } else if ($checked.val() == 'undefined') {
+        $.show('everything!')
+        console.log("ok annie")
+    }
 });
