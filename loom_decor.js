9ea73cc254f6722259c16d443eab8dc9b6c27ce2
Adam D. White <adamwhite82@gmail.com>
2018-03-12 00:35:38 -0500

    jQuery sorted, incl. === undefined

--- loom_decor.js
+++ loom_decor.js
@@ -5,20 +5,35 @@ console.log('loom decor js')
 // Room checkbox filtering function
 var $rooms = $('input[name="roomcheckbox"]');
 
-$rooms.change(function() {
-    var $checked = $rooms.filter(':checked');
-    console.log($checked.val());
-    console.log("goodbye world");
-});
-
 // $rooms.change(function() {
 //     var $checked = $rooms.filter(':checked');
-//     if ($checked.val() == 'kitchen') {
-//         $('div[type="kitchen-content"]').hide;
-//         $('.').show;
-
-//     } else if ($checked.val() == 'undefined') {
-//         $.show('everything!')
-//         console.log("ok annie")
-//     }
+//     console.log($checked.val());
+//     console.log("goodbye world");
 // });
+
+$rooms.change(function() {
+    var $checked = $rooms.filter(':checked');
+    if ($checked.val() == 'bathroom') {
+        console.log($checked.val());
+        $("div.bedroom-content, div.kitchen-content, div.living-room-content, div.study-content").hide();
+        // repeat this block for each room, for now...
+    } else if ($checked.val() == 'bedroom') {
+        console.log($checked.val());
+        $("div.bathroom-content, div.kitchen-content, div.living-room-content, div.study-content").hide();
+    } else if ($checked.val() == 'kitchen') {
+        console.log($checked.val());
+        $("div.bathroom-content, div.bedroom-content, div.living-room-content, div.study-content").hide();
+    } else if ($checked.val() == 'living') {
+        console.log($checked.val());
+        $("div.bathroom-content, div.bedroom-content, div.kitchen-content, div.study-content").hide();
+    } else if ($checked.val() == 'study') {
+        console.log($checked.val());
+        $("div.bathroom-content, div.bedroom-content, div.kitchen-content, div.living-room-content").hide();
+    }
+    // must have a data type in order to use '===' effectively
+    else if ($checked.val() === undefined) {
+        console.log($checked.val());
+        $("div.bathroom-content, div.bedroom-content, div.kitchen-content, div.living-room-content, div.study-content").show();
+        //     console.log("ok annie")
+    }
+});
