77db2bc754c992768e12152719a89dce032907f1
Adam D. White <adamwhite82@gmail.com>
2018-03-11 20:06:08 -0500

    roll back to last night to test .change  functions

--- loom_decor.js
+++ loom_decor.js
@@ -5,21 +5,20 @@ console.log('loom decor js')
 // Room checkbox filtering function
 var $rooms = $('input[name="roomcheckbox"]');
 
+$rooms.change(function() {
+    var $checked = $rooms.filter(':checked');
+    console.log($checked.val());
+    console.log("goodbye world");
+});
 
 // $rooms.change(function() {
 //     var $checked = $rooms.filter(':checked');
-//     console.log($checked.val());
-//     console.log("goodbye world");
+//     if ($checked.val() == 'kitchen') {
+//         $('div[type="kitchen-content"]').hide;
+//         $('.').show;
+
+//     } else if ($checked.val() == 'undefined') {
+//         $.show('everything!')
+//         console.log("ok annie")
+//     }
 // });
-
-$rooms.change(function() {
-    var $checked = $rooms.filter(':checked');
-    if ($checked.val() == 'kitchen') {
-        $(".kitchen-content").hide;
-        $('.').show;
-
-    } else if ($checked.val() == 'undefined') {
-        $.show('everything!')
-        console.log("ok annie")
-    }
-});
